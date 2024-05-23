0a0322463ca53833b6308af2c5f87c8876b466a9
thasnishihab <thasnikalathil93@gmail.com>
2024-05-23 14:00:30 +0400

    settings heroku update2

--- student_grades/urls.py
+++ student_grades/urls.py
@@ -38,5 +38,5 @@ urlpatterns = [
     path('grades/', include(grades.urls))
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
